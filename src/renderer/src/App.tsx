c874e927174fdedc2614c4e28d5a6952aeffe48e
arora-archit <arora.archit19@gmail.com>
2025-02-22 18:27:35 +0530

    Fix: ImageProvider doesn't wrap everthing now

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -5,41 +5,53 @@ import Feed from '@renderer/components/Feed'
 import Sync from '@renderer/components/Sync'
 import Settings from '@renderer/components/Settings'
 import Home from '@renderer/components/Home'
-import { ImageProvider } from './context/imageContext';
+import { ImageProvider } from './context/imageContext'
 
 function App(): React.JSX.Element {
   return (
-    <ImageProvider>
-      <div className="flex min-h-screen flex-row">
-        <nav className={'flex flex-col gap-y-4 bg-gray-700 px-4 py-3'}>
-          <Link to={'/'} className={'flex flex-row gap-x-2'}>
-            <House />
-            <p>Home</p>
-          </Link>
-          <Link to={'/feed'} className={'flex flex-row gap-x-2'}>
-            <Rss />
-            <p>Feed</p>
-          </Link>
-          <Link to={'/sync'} className={'flex flex-row gap-x-2'}>
-            <Cloud />
-            <p>Sync</p>
-          </Link>
-          <Link to={'/settings'} className={'flex flex-row gap-x-2'}>
-            <SettingsIcon />
-            <p>Settings</p>
-          </Link>
-        </nav>
+    <div className="flex min-h-screen flex-row">
+      <nav className={'flex flex-col gap-y-4 bg-gray-700 px-4 py-3'}>
+        <Link to={'/'} className={'flex flex-row gap-x-2'}>
+          <House />
+          <p>Home</p>
+        </Link>
+        <Link to={'/feed'} className={'flex flex-row gap-x-2'}>
+          <Rss />
+          <p>Feed</p>
+        </Link>
+        <Link to={'/sync'} className={'flex flex-row gap-x-2'}>
+          <Cloud />
+          <p>Sync</p>
+        </Link>
+        <Link to={'/settings'} className={'flex flex-row gap-x-2'}>
+          <SettingsIcon />
+          <p>Settings</p>
+        </Link>
+      </nav>
 
-        <div className={'flex flex-1 flex-col items-center justify-center p-4'}>
-          <Routes>
-            <Route path={'/'} element={<Home />} />
-            <Route path="/feed" element={<Feed />} />
-            <Route path="/sync" element={<Sync />} />
-            <Route path="/settings" element={<Settings />} />
-          </Routes>
-        </div>
+      <div className={'flex flex-1 flex-col items-center justify-center p-4'}>
+        <Routes>
+          <Route
+            path={'/'}
+            element={
+              <ImageProvider>
+                <Home />
+              </ImageProvider>
+            }
+          />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/sync" element={<Sync />} />
+          <Route
+            path="/settings"
+            element={
+              <ImageProvider>
+                <Settings />
+              </ImageProvider>
+            }
+          />
+        </Routes>
       </div>
-    </ImageProvider>
+    </div>
   )
 }
 
--- src/renderer/src/components/Settings.tsx
+++ src/renderer/src/components/Settings.tsx
@@ -1,4 +1,4 @@
-// import { useContext } from 'react'
+import { useContext } from 'react'
 import { useImageContext } from '../context/imageContext'
 
 const Settings: React.FC = () => {
