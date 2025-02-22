941fa417eb6359ba61efe4430c90a9a47a164454
TheChilledBuffalo <abhishekgs.tvm@gmail.com>
2025-02-22 13:01:21 +0530

    add sidebar and routers

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Link, Routes, Route } from 'react-router-dom'
+import { Cloud, House, SettingsIcon } from 'lucide-react'
+import Feed from '@renderer/components/Feed'
+import Sync from '@renderer/components/Sync'
+import Settings from '@renderer/components/Settings'
 
 function App(): React.JSX.Element {
   return (
-    <div>
-      <h1 className="text-6xl">Welcome to WallDo!</h1>
+    <div className="flex h-screen flex-row">
+      <nav className={'flex flex-col gap-y-4 bg-gray-700 px-4 py-3'}>
+        <Link to={'/'} className={'flex flex-row gap-x-2'}>
+          <House />
+          <p>Home</p>
+        </Link>
+        <Link to={'/sync'} className={'flex flex-row gap-x-2'}>
+          <Cloud />
+          <p>Sync</p>
+        </Link>
+        <Link to={'/settings'} className={'flex flex-row gap-x-2'}>
+          <SettingsIcon />
+          <p>Settings</p>
+        </Link>
+      </nav>
+
+      <div className={'flex flex-1 flex-col items-center justify-center p-4'}>
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/sync" element={<Sync />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </div>
     </div>
   )
 }
